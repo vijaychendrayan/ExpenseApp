bbc51bfb2890369acfb83b419ceeb77bea1ebcc7
Vijay Chendrayan Chandrasekar <vijaychendrayan@gmail.com>
2023-05-12 22:49:34 +0530

    has issue

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,44 +3,12 @@ import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 import { GlobalStyles } from "../../constants/styles";
 
-const DUMMY_EXPENSES = [
-    {
-        id: 'e1',
-        description: 'A pair of shoes',
-        amount: 59.99,
-        date: new Date('2023-01-01')
-    },
-    {
-        id: 'e2',
-        description: 'A pair of Socks',
-        amount: 9.99,
-        date: new Date('2023-01-02')
-    },
-    {
-        id: 'e3',
-        description: 'A pair of Shirts',
-        amount: 159.99,
-        date: new Date('2023-01-03')
-    },
-    {
-        id: 'e4',
-        description: 'A pair of spoon',
-        amount: 5.99,
-        date: new Date('2023-01-04')
-    },
-    {
-        id: 'e5',
-        description: 'A pair of plate',
-        amount: 25.99,
-        date: new Date('2023-01-05')
-    },
-]
 
 function ExpensesOutput({expenses, expensesPeriod}){
     return( 
         <View style={styles.container}> 
-            <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod}/>
-            <ExpensesList expenses={DUMMY_EXPENSES} />
+            <ExpensesSummary expenses={expenses} periodName={expensesPeriod}/>
+            <ExpensesList expenses={expenses} />
         </View>
     )
     
---
+++ store/expenses-context.js
@@ -0,0 +1,90 @@
+import { createContext, useReducer } from "react";
+
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        description: 'A pair of shoes',
+        amount: 59.99,
+        date: new Date('2023-01-01')
+    },
+    {
+        id: 'e2',
+        description: 'A pair of Socks',
+        amount: 9.99,
+        date: new Date('2023-01-02')
+    },
+    {
+        id: 'e3',
+        description: 'A pair of Shirts',
+        amount: 159.99,
+        date: new Date('2023-01-03')
+    },
+    {
+        id: 'e4',
+        description: 'A pair of spoon',
+        amount: 5.99,
+        date: new Date('2023-01-04')
+    },
+    {
+        id: 'e5',
+        description: 'A pair of plate',
+        amount: 25.99,
+        date: new Date('2023-01-05')
+    },
+]
+
+
+const ExpensesContext = createContext({
+    expenses: [],
+    addExpense: ({description, amout, date}) => {},
+    deleteExpense: (id) => {},
+    updateExpense: (id, {description, amout, date}) => {},
+});
+
+function expensesReducer(state, action){
+    switch(action.type){
+        case 'ADD': 
+            const id = new Date().toString() + Math.random().toString();
+            return [{...action.payload, id: id},...state]
+        case 'UPDATE':
+            const updatableExpenseIndex = state.findIndex((expense)=> expense.id === action.payload.id);
+            const updatableExpense = state[updatableExpenseIndex]
+            const updatedItem = {...updatableExpense, ...action.payload.data}
+            const updatedExpenses = [...state];
+            updatedExpenses[updatableExpenseIndex] = updatedItem;
+            return updatedExpenses;
+        case 'DELETE':
+            return state.filter((expense) => expense.id !== action.payload.id)
+        default:
+            return state;
+    }
+}
+
+function ExpensesContextProvider(){
+    const [expensesState, dispatch]=useReducer(expensesReducer, DUMMY_EXPENSES);
+
+    function addExpense(expenseData){
+        dispatch({type: 'ADD', payload: expenseData})
+
+    }
+
+    function deleteExpense(id){
+        dispatch({type: 'DELETE', payload: id})
+    }
+
+    function updateExpense(id, expenseData){
+        dispatch({type: 'UPDATE', payload:{id: id, data: expenseData}})
+    }
+
+    const value={
+        expenses: expensesState,
+        addExpense: addExpense,
+        deleteExpense: deleteExpense,
+        updateExpense : updateExpense,
+    }
+    return (
+        <ExpensesContext.Provider value={value}>{children}</ExpensesContext.Provider>
+    )
+}
+
+export default ExpensesContextProvider;
