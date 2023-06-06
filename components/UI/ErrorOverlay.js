acaea1b5db2e3c9dd3e636ef80c9a7745e8ee76d
Vijay Chendrayan Chandrasekar <vijaychendrayan@gmail.com>
2023-06-06 10:46:42 +0530

    Error Overlay 2

--- components/UI/ErrorOverlay.js
+++ components/UI/ErrorOverlay.js
@@ -1,36 +1,39 @@
-import { View, StyleSheet, Text,Button } from "react-native"
+import { View, StyleSheet, Text, Button } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 
-function ErrorOverlay({message, onConfirm}){
-    return <View style={styles.container}>
-                <Text style={[styles.text,styles.title]}> An error occured </Text>
-                <Text style={styles.text}>{message}</Text>
-                <Button onPress={onConfirm} title="Press Me"> Okay</Button>
+function ErrorOverlay({ message, onConfirm }) {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.text, styles.title]}> An error occured </Text>
+      <Text style={styles.text}>{message}</Text>
+      <Button onPress={onConfirm} title="Press Me">
+        {" "}
+        Okay
+      </Button>
     </View>
+  );
 }
 
 export default ErrorOverlay;
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 24,
-        backgroundColor: GlobalStyles.colors.primary700
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
 
-    },  
-
-    text:{
-        textAlign: 'center',
-        marginBottom: 8
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
-    message:{
-        fontSize: 14,
-        
-    }
-})
+  text: {
+    textAlign: "center",
+    marginBottom: 8,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  message: {
+    fontSize: 14,
+  },
+});
--- store/expenses-context.js
+++ store/expenses-context.js
@@ -39,70 +39,72 @@ import { createContext, useReducer } from "react";
 //     },
 // ]
 
-
 export const ExpensesContext = createContext({
-    expenses: [ ],
-    addExpense: ({description, amout, date}) => {},
-    setExpenses: (expense)=> {},
-    deleteExpense: (id) => {},
-    updateExpense: (id, {description, amout, date}) => {},
+  expenses: [],
+  addExpense: ({ description, amout, date }) => {},
+  setExpenses: (expense) => {},
+  deleteExpense: (id) => {},
+  updateExpense: (id, { description, amout, date }) => {},
 });
 
-function expensesReducer(state, action){
-    switch(action.type){
-        case 'ADD': 
-            // const id = new Date().toString() + Math.random().toString();
-            return [action.payload ,...state]
-        case 'SET':
-            const inverted = action.payload.reverse();
-            return inverted;
-        case 'UPDATE':
-            const updatableExpenseIndex = state.findIndex((expense)=> expense.id === action.payload.id);
-            const updatableExpense = state[updatableExpenseIndex]
-            const updatedItem = {...updatableExpense, ...action.payload.data}
-            const updatedExpenses = [...state];
-            updatedExpenses[updatableExpenseIndex] = updatedItem;
-            return updatedExpenses;
-        case 'DELETE':
-            // console.log(state)
-            // console.log('expense-context.js : Expense Reducer -> DELETE -> action.payload.id : ', action.payload)
-            // return state.filter((expense) => expense.id !== action.payload.id)
-            return state.filter((expense) => expense.id !== action.payload)
-        default:
-            return state;
-    }
+function expensesReducer(state, action) {
+  switch (action.type) {
+    case "ADD":
+      // const id = new Date().toString() + Math.random().toString();
+      return [action.payload, ...state];
+    case "SET":
+      const inverted = action.payload.reverse();
+      return inverted;
+    case "UPDATE":
+      const updatableExpenseIndex = state.findIndex(
+        (expense) => expense.id === action.payload.id
+      );
+      const updatableExpense = state[updatableExpenseIndex];
+      const updatedItem = { ...updatableExpense, ...action.payload.data };
+      const updatedExpenses = [...state];
+      updatedExpenses[updatableExpenseIndex] = updatedItem;
+      return updatedExpenses;
+    case "DELETE":
+      // console.log(state)
+      // console.log('expense-context.js : Expense Reducer -> DELETE -> action.payload.id : ', action.payload)
+      // return state.filter((expense) => expense.id !== action.payload.id)
+      return state.filter((expense) => expense.id !== action.payload);
+    default:
+      return state;
+  }
 }
 
-function ExpensesContextProvider({children}){
-    const [expensesState, dispatch]=useReducer(expensesReducer, []);
+function ExpensesContextProvider({ children }) {
+  const [expensesState, dispatch] = useReducer(expensesReducer, []);
 
-    function addExpense(expenseData){
-        dispatch({type: 'ADD', payload: expenseData})
+  function addExpense(expenseData) {
+    dispatch({ type: "ADD", payload: expenseData });
+  }
 
-    }
+  function setExpenses(expenses) {
+    dispatch({ type: "SET", payload: expenses });
+  }
 
-    function setExpenses(expenses){
-        dispatch({type: 'SET', payload: expenses});
-    }
+  function deleteExpense(id) {
+    dispatch({ type: "DELETE", payload: id });
+  }
 
-    function deleteExpense(id){
-        dispatch({type: 'DELETE', payload: id})
-    }
+  function updateExpense(id, expenseData) {
+    dispatch({ type: "UPDATE", payload: { id: id, data: expenseData } });
+  }
 
-    function updateExpense(id, expenseData){
-        dispatch({type: 'UPDATE', payload:{id: id, data: expenseData}})
-    }
-
-    const value={
-        expenses: expensesState,
-        addExpense: addExpense,
-        setExpenses: setExpenses,
-        deleteExpense: deleteExpense,
-        updateExpense: updateExpense,
-    }
-    return (
-        <ExpensesContext.Provider value={value}>{children}</ExpensesContext.Provider>
-    )
+  const value = {
+    expenses: expensesState,
+    addExpense: addExpense,
+    setExpenses: setExpenses,
+    deleteExpense: deleteExpense,
+    updateExpense: updateExpense,
+  };
+  return (
+    <ExpensesContext.Provider value={value}>
+      {children}
+    </ExpensesContext.Provider>
+  );
 }
 
 export default ExpensesContextProvider;
