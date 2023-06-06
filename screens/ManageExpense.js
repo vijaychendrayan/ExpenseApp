7082f09bf9ff873dc6490fe4b323500e2d4b800e
Vijay Chendrayan Chandrasekar <vijaychendrayan@gmail.com>
2023-06-06 10:46:23 +0530

    Error Overlay

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -8,110 +8,111 @@ import { storeExpense, updateExpense, deleteExpense } from "../util/http";
 import LoadingOverlay from "../components/UI/LoadingOverlay";
 import ErrorOverlay from "../components/UI/ErrorOverlay";
 
-function ManageExpense({route, navigation}){
-    const [isSubmitting, setIsSubmitting] = useState(false)
-    const [error, setError] = useState();
-    const expensesCtx = useContext(ExpensesContext)
-    const editedExpenseID = route.params?.expenseId;
-    const isEditing = !!editedExpenseID;
-    const selectedExpense = expensesCtx.expenses.find(expense => expense.id === editedExpenseID)
+function ManageExpense({ route, navigation }) {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState();
+  const expensesCtx = useContext(ExpensesContext);
+  const editedExpenseID = route.params?.expenseId;
+  const isEditing = !!editedExpenseID;
+  const selectedExpense = expensesCtx.expenses.find(
+    (expense) => expense.id === editedExpenseID
+  );
 
-    useLayoutEffect(()=>{
-        navigation.setOptions({
-            title: isEditing ? 'Edit Expense' : 'Add Expense',
-        });
-    },[navigation, isEditing]);
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
 
-    async function deleteExpenseHandler(){
-        // console.log('ManageExpenses.js : editedExpenseID : ',editedExpenseID)
-        setIsSubmitting(true)
-        try{
-            await deleteExpense(editedExpenseID)
-            expensesCtx.deleteExpense(editedExpenseID)
-            navigation.goBack();
-        }catch(error){
-            setError('Could not delete expense - pls try again later!')
-            setIsSubmitting(false)
-        }
-
+  async function deleteExpenseHandler() {
+    // console.log('ManageExpenses.js : editedExpenseID : ',editedExpenseID)
+    setIsSubmitting(true);
+    try {
+      await deleteExpense(editedExpenseID);
+      expensesCtx.deleteExpense(editedExpenseID);
+      navigation.goBack();
+    } catch (error) {
+      setError("Could not delete expense - pls try again later!");
+      setIsSubmitting(false);
     }
+  }
 
-    function cancelHandler(){
-        navigation.goBack();
+  function cancelHandler() {
+    navigation.goBack();
+  }
 
+  async function confirmHandler(expenseData) {
+    setIsSubmitting(true);
+    try {
+      if (isEditing) {
+        expensesCtx.updateExpense(editedExpenseID, expenseData);
+        await updateExpense(editedExpenseID, expenseData);
+      } else {
+        const id = await storeExpense(expenseData);
+        expensesCtx.addExpense({ ...expenseData, id: id });
+      }
+      navigation.goBack();
+    } catch (error) {
+      setError("Could not save expense - pls try again later!");
+      setIsSubmitting(false);
     }
-     
-    async function confirmHandler(expenseData){
-        setIsSubmitting(true);
-        try{
+  }
 
-            if(isEditing){
-                expensesCtx.updateExpense(editedExpenseID,expenseData);
-                await updateExpense(editedExpenseID,expenseData);
-            }else{
-                const id = await storeExpense(expenseData);
-                expensesCtx.addExpense({...expenseData, id: id});
-            }
-            navigation.goBack();
-        }catch(error){
-            setError('Could not save expense - pls try again later!')
-            setIsSubmitting(false)
+  function errorHandler() {
+    setError(null);
+  }
 
-        }
-    }
+  if (error && !isSubmitting) {
+    return <ErrorOverlay message={error} onConfirm={errorHandler} />;
+  }
+  if (isSubmitting) {
+    return <LoadingOverlay />;
+  }
 
-    function errorHandler(){
-        setError(null)
-    }
-
-    if( error && !isSubmitting){
-        return <ErrorOverlay message={error} onConfirm={errorHandler}/>
-    }
-    if (isSubmitting){
-
-        return <LoadingOverlay />
-    }
-
-    return (
-        <View style={styles.container}>
-            <ExpenseForm    onCancel={cancelHandler} 
-                            onSubmit={confirmHandler}
-                            submitButtonLabel={isEditing ? 'Update':'Add'}
-                            defaultValues={selectedExpense}
-            />                     
-            {isEditing && (
-                <View style={styles.deleteContainer}>
-                    <IconButton icon="trash" color={GlobalStyles.colors.error500} size={36} onPress={deleteExpenseHandler}/>
-                </View>
-                )
-            }
-
+  return (
+    <View style={styles.container}>
+      <ExpenseForm
+        onCancel={cancelHandler}
+        onSubmit={confirmHandler}
+        submitButtonLabel={isEditing ? "Update" : "Add"}
+        defaultValues={selectedExpense}
+      />
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
         </View>
-    )
+      )}
+    </View>
+  );
 }
 
 export default ManageExpense;
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        padding: 24,
-        backgroundColor: GlobalStyles.colors.primary700,
-    },
-    buttons:{
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    button:{
-        minWidth: 120,
-        marginHorizontal:8,
-    },
-    deleteContainer:{
-        marginTop: 16,
-        paddingTop: 8,
-        borderTopWidth: 2,
-        borderTopColor: GlobalStyles.colors.primary200,
-        alignItems: 'center'
-    }
-})
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
--- util/http.js
+++ util/http.js
@@ -1,40 +1,41 @@
 import axios from "axios";
 
-BACKEND_URL = 'https://expensetracker-a454b-default-rtdb.asia-southeast1.firebasedatabase.app'
+BACKEND_URL =
+  "https://expensetracker-a454b-default-rtdb.asia-southeast1.firebasedatabase.app";
 
-export async function storeExpense(expenseData){
-    const response = await axios.post(BACKEND_URL+'/expenses.json', expenseData);
-    const id  = response.data.name;
-    return id;
+export async function storeExpense(expenseData) {
+  const response = await axios.post(
+    BACKEND_URL + "/expenses.json",
+    expenseData
+  );
+  const id = response.data.name;
+  return id;
 }
 
-export  async function fetchExpenses(){
-    console.log('Fetching data.... start');
-    const response = await axios.get(BACKEND_URL + '/expenses.json')
-                            .catch(error => console.log(error));
-   
-    
-    const expenses = []
-    for ( const key in response.data){
-        const expenseObj = {
-            id: key,
-            amount: response.data[key].amount,
-            date: new Date(response.data[key].date),
-            description: response.data[key].description
-        };
-        expenses.push(expenseObj);
-    }
-    console.log('Fetching data.... end');
-   return expenses;
+export async function fetchExpenses() {
+  console.log("Fetching data.... start");
+  const response = await axios
+    .get(BACKEND_URL + "/expenses.json")
+    .catch((error) => console.log(error));
 
+  const expenses = [];
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    };
+    expenses.push(expenseObj);
+  }
+  console.log("Fetching data.... end");
+  return expenses;
 }
 
-export  function updateExpense(id, expenseData){
-    return axios.put(BACKEND_URL+ `/expenses/${id}.json`, expenseData)
+export function updateExpense(id, expenseData) {
+  return axios.put(BACKEND_URL + `/expenses/${id}.json`, expenseData);
 }
 
-export  function deleteExpense(id){
-    return axios.delete(BACKEND_URL+ `/expenses/${id}.json`)
-    
+export function deleteExpense(id) {
+  return axios.delete(BACKEND_URL + `/expenses/${id}.json`);
 }
-
